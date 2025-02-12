faaf2581666bb3db49abe9e550b89cfc496bd6c2
Lachlan Fitzgerald <lachlan.fitzgerald1@gmail.com>
2025-02-12 11:05:59 +1000

    Changed PWM values

--- src/main.cpp
+++ src/main.cpp
@@ -28,14 +28,14 @@ int counter = 0; //when counter = 2880 stop
 
 int start_delay = 12; // Specify delay in hours 
 
-void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int rpm);
+void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int duty);
 
 time_t getTeensy3Time() {
 	return Teensy3Clock.get();
 }
 
 void setup() {
-
+  analogWriteResolution(10);
   Serial.begin(9600);
   alarm.setRtcTimer(0,0,2); // hour, min, sec (0,10,0)
   setSyncProvider(getTeensy3Time); // Guess??????? it gets the time from the Serial5 monitor
@@ -100,7 +100,7 @@ void setRTC(void) {
 /**stepper motor control
 
 */
-void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int rpm) { //todo: direction
+void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int duty) { //todo: direction
   //enable the stepper motor pin to hold the torque
   //int timer = 0;
   digitalWrite(en_pin, HIGH);
@@ -111,7 +111,7 @@ void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int rpm) { //t
   } else{
     digitalWrite(dir_pin, HIGH);
   }
-  if (rpm == 0) {
+  if (duty == 0) {
     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
     //disable the drive pin
     analogWrite(pin, 0);
@@ -119,10 +119,7 @@ void stepper_act(int pin, int dir_pin, int clockwise, int en_pin, int rpm) { //t
     digitalWrite(en_pin, LOW);
   } else {
       digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-      //1/256 micro stepping mode: 1 rotation = 51,200 (200 x 256) pulses. 51.2 kHz = 60 rpm, 512 kHz = 600 rpm
-      analogWriteFrequency(pin, rpm*51200/60); // 83rpm x 51200/60
-      // analogWriteResolution(12);
-      analogWrite(pin, 120);    
-      delay(1000);
+      analogWriteFrequency(pin, 375000); 
+      analogWrite(pin, duty * 1023 / 100);    
     }
 }
